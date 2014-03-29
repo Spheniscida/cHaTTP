cf36a7b9915ed7ce2dd7c389539b6f65ca0a8760
lbo <lbo@spheniscida.de>
2014-03-29 13:56:33 +0100

    Changed environment variables in the broker configuration
    (according to 5e70947)

--- message-broker/src/conf.cpp
+++ message-broker/src/conf.cpp
@@ -4,57 +4,62 @@
 
 using std::getenv;
 
+const char* mesg_relay_addr_env_var  = "CHATTP_MESG_RELAY_ADDR";
+const char* mesg_relay_conn_env_var  = "CHATTP_MESG_RELAY_CONN_TYPE";
+const char* persistence_addr_env_var = "CHATTP_PERSISTENCE_LAYER_ADDRESS";
+const char* persistence_conn_env_var = "CHATTP_PERSISTENCE_LAYER_CONN_TYPE";
+
 /**
  * @brief Fetch configuration from environment.
  *
  * This class configures the message broker. It uses the following environment variables:
  *
- * - MESG_RELAY_ADDR - The socket address of the message relay daemon
- * - MESG_RELAY_CONN_TYPE - ("UNIX"/"UDP") How to connect to that daemon.
- * - PERSISTENCE_LAYER_ADDRESS - The socket address of the persistence layer daemon
- * - PERSISTENCE_LAYER_CONN_TYPE - ("UNIX"/"UDP") How to connect to that daemon.
+ * - CHATTP_MESG_RELAY_ADDR - The socket address of the message relay daemon
+ * - CHATTP_MESG_RELAY_CONN_TYPE - ("UNIX"/"UDP") How to connect to that daemon.
+ * - CHATTP_PERSISTENCE_LAYER_ADDRESS - The socket address of the persistence layer daemon
+ * - CHATTP_PERSISTENCE_LAYER_CONN_TYPE - ("UNIX"/"UDP") How to connect to that daemon.
  *
  */
 brokerSettings::brokerSettings(void)
 {
     // Message relay configuration
-    if ( getenv("MESG_RELAY_ADDR") )
-	message_relay_address = getenv("MESG_RELAY_ADDR");
+    if ( getenv(mesg_relay_addr_env_var) )
+	message_relay_address = getenv(mesg_relay_addr_env_var);
     else
-	throw brokerError(errorType::configurationError,"There is no MESG_RELAY_ADDR environment variable");
+	throw brokerError(errorType::configurationError,"There is no CHATTP_MESG_RELAY_ADDR environment variable");
 
 
-    if ( getenv("MESG_RELAY_CONN_TYPE") )
+    if ( getenv(mesg_relay_conn_env_var) )
     {
-	string conn_type(getenv("MESG_RELAY_CONN_TYPE"));
+	string conn_type(getenv(mesg_relay_conn_env_var));
 	if (conn_type == "UNIX")
 	    message_relay_connection_type = connectionType::UNIX;
 	else if (conn_type == "UDP")
 	    message_relay_connection_type = connectionType::UDP;
 	else
-	    throw brokerError(errorType::configurationError,"There's something wrong with the MESG_RELAY_CONN_TYPE environment variable");
+	    throw brokerError(errorType::configurationError,"There's something wrong with the CHATTP_MESG_RELAY_CONN_TYPE environment variable");
     }
 
     // Persistence layer (data source) configuration
-    if ( getenv("PERSISTENCE_LAYER_ADDRESS") )
-	persistence_layer_address = getenv("PERSISTENCE_LAYER_ADDRESS");
+    if ( getenv(persistence_addr_env_var) )
+	persistence_layer_address = getenv(persistence_addr_env_var);
     else
-	throw brokerError(errorType::configurationError,"There is no PERSISTENCE_LAYER_ADDRESS environment variable");
+	throw brokerError(errorType::configurationError,"There is no CHATTP_PERSISTENCE_LAYER_ADDRESS environment variable");
 
 
-    if ( getenv("PERSISTENCE_LAYER_CONN_TYPE") )
+    if ( getenv(persistence_conn_env_var) )
     {
-	string conn_type(getenv("PERSISTENCE_LAYER_CONN_TYPE"));
+	string conn_type(getenv(persistence_conn_env_var));
 
 	if ( conn_type == "UDP" )
 	    persistence_layer_connection_type = connectionType::UDP;
 	else if ( conn_type == "UNIX" )
 	    persistence_layer_connection_type = connectionType::UNIX;
 	else
-	    throw brokerError(errorType::configurationError,"There's something wrong with the PERSISTENCE_LAYER_CONN_TYPE environment variable");
+	    throw brokerError(errorType::configurationError,"There's something wrong with the CHATTP_PERSISTENCE_LAYER_CONN_TYPE environment variable");
     }
     else
-	throw brokerError(errorType::configurationError,"There is no PERSISTENCE_LAYER_CONN_TYPE environment variable");
+	throw brokerError(errorType::configurationError,"There is no CHATTP_PERSISTENCE_LAYER_CONN_TYPE environment variable");
 }
 
 connectionInformation brokerSettings::getMessageRelayAddress(void)
