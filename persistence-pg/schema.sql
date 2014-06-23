c24ae5cb132991264260b6055815816ecd8c7625
lbo <lbo@spheniscida.de>
2014-06-23 14:28:13 +0200

    SQL schema for persistence-pg

---
+++ persistence-pg/schema.sql
@@ -0,0 +1,57 @@
+
+-- users table.
+CREATE TABLE chattp_users
+(
+    user_id SERIAL NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    user_email VARCHAR(50) NOT NULL,
+    user_password VARCHAR(72) NOT NULL, -- max. bf password length
+
+    PRIMARY KEY (user_id),
+    UNIQUE (user_id)
+);
+
+-- We select mainly over user_name
+CREATE INDEX chattp_users_user_name_ix ON chattp_users (user_name);
+
+CREATE TABLE chattp_messages
+(
+    receiver INTEGER NOT NULL,
+    sender INTEGER NOT NULL,
+    timestamp VARCHAR(20) NOT NULL, -- The timestamp is handled only by the client, we don't need to parse it
+    group_message BOOLEAN,
+    body TEXT,
+
+    FOREIGN KEY (receiver) REFERENCES chattp_users(user_id),
+    FOREIGN KEY (sender) REFERENCES chattp_users(user_id)
+);
+
+-- We select mainly over receiver
+CREATE INDEX chattp_messages_receiver_ix ON chattp_messages (receiver);
+
+CREATE TABLE chattp_brokers
+(
+    broker_id SERIAL NOT NULL,
+    broker_name VARCHAR(75) NOT NULL,
+
+    PRIMARY KEY (broker_id),
+    UNIQUE (broker_id),
+    UNIQUE (broker_name)
+);
+
+-- Our main constraint will be the name
+CREATE INDEX chattp_brokers_broker_name_ix ON chattp_brokers (broker_name);
+
+CREATE TABLE chattp_locations
+(
+    user_id INTEGER NOT NULL,
+    broker_id INTEGER NOT NULL,
+    channel_id CHARACTER(64) NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES chattp_users(user_id),
+    FOREIGN KEY (broker_id) REFERENCES chattp_brokers(broker_id),
+    UNIQUE (channel_id)
+);
+
+CREATE INDEX chattp_locations_user_id_ix ON chattp_locations (user_id);
+
